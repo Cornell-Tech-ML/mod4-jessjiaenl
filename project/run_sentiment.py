722ae44ae8c084c8f2dfe908e16bc456a8dca350
JIAENL <128503379+jessjiaenl@users.noreply.github.com>
2024-12-09 11:09:14 -0500

    feat(run_sentiment): implemented run_sentiment and increased requirements dataset version

--- project/run_sentiment.py
+++ project/run_sentiment.py
@@ -34,8 +34,7 @@ class Conv1d(minitorch.Module):
         self.bias = RParam(1, out_channels, 1)
 
     def forward(self, input):
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        return minitorch.conv1d(input, self.weights.value) + self.bias.value
 
 
 class CNNSentimentKim(minitorch.Module):
@@ -61,15 +60,32 @@ class CNNSentimentKim(minitorch.Module):
     ):
         super().__init__()
         self.feature_map_size = feature_map_size
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        self.dropout = dropout
+        
+        self.conv1 = Conv1d(embedding_size, feature_map_size, filter_sizes[0])
+        self.conv2 = Conv1d(embedding_size, feature_map_size, filter_sizes[1])
+        self.conv3 = Conv1d(embedding_size, feature_map_size, filter_sizes[2])
+        self.lin = Linear(self.feature_map_size, 1)
 
     def forward(self, embeddings):
         """
         embeddings tensor: [batch x sentence length x embedding dim]
         """
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        # conv1.forward's input : batch x in_channel x h x w, so need to make embedding dim the 1st dim
+        embeddings = embeddings.permute(0, 2, 1)
+
+        conv1 = self.conv1.forward(embeddings).relu()
+        conv2 = self.conv2.forward(embeddings).relu()
+        conv3 = self.conv3.forward(embeddings).relu()
+
+        # max over time for each channel
+        out = (minitorch.max(conv1, 2) + minitorch.max(conv2, 2) + minitorch.max(conv3, 2)).view(embeddings.shape[0], self.feature_map_size)
+        
+        # linear then relu then dropout
+        out = self.lin.forward(out).relu()
+        out = minitorch.dropout(out, rate=self.dropout)
+
+        return out.sigmoid()
 
 
 # Evaluation helper methods
